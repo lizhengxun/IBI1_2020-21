89e401613b1c555b6cfa7689fe5b0e14811e71d2
李政洵 <lizhengxun@lizhengxundeMacBook-Pro.local>
2021-03-10 09:48:50 +0800

    r_rate.py

--- Practical5/r_rate.py
+++ Practical5/r_rate.py
@@ -1,9 +1,10 @@
-#the initial number is 84 and r rate is 1.2
+#the initial number is 84 
 n=84
-r=1.2
+#input r rate
+r=input ("r rate is:")
 j=0
 #5 round of injection
 while j<=4:
-	n=n+n*1.2
+	n=n+n*r
 	j+=1
-print("if r rate is 1.2, and initial number is 84, the total number of individuals infected after 5 generations is",n)
+print("r rate is:",r,"the total number of individuals infected after 5 generations",n)
