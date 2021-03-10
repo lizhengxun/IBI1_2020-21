df94a2d2c924895d3f8a5c5e559028be67627dbd
李政洵 <lizhengxun@lizhengxundeMacBook-Pro.local>
2021-03-10 09:35:36 +0800

    variables.py

--- Practical5/variables.py
+++ Practical5/variables.py
@@ -3,8 +3,16 @@ b=190784
 c=100321
 d=c-a
 e=b-a
+print ("d=",d)
+print ("e=",e)
 if d>e:
-	print (d,"is greater")
+	print "d is greater"
 else:
-	print (e,"is greater")
+	print "e is greater"
 
+X=True
+Y=False
+Z=(X and not Y) or (Y and not X)
+W=(X !=Y)
+print (Z)
+print (W)
