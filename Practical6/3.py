78bbf4520f313fe2041d27cd919bfe4f79096c98
李政洵 <lizhengxun@lizhengxundeMacBook-Pro.local>
2021-03-17 09:53:33 +0800

    3.py

---
+++ Practical6/3.py
@@ -0,0 +1,20 @@
+gene_lengths=[9410, 394141, 4442, 105338, 19149, 76779, 126550, 36296, 842, 159]
+exon_counts=[51, 1142, 42, 216, 25, 650, 32533, 57, 1, 523]
+L = []
+for i in range(0,10):
+    a = gene_lengths[i]/exon_counts[i]
+    L.append(a)
+import matplotlib.pyplot as plt
+import numpy as np
+length = L
+plt.boxplot(length,
+	    vert=True,
+            whis=1.5,
+            patch_artist = True,
+	    meanline = False,
+            showbox = True,
+            showcaps = True,
+            showfliers = True,
+            notch = False,
+              )
+plt.show()
