c2e49ac26a5ff50924882d44fe362e211178864e
lizhengxun <zhengxun.20@intl.zju.edu.cn>
2021-04-01 10:53:49 +0800

    1

---
+++ Practical8/DNA_to_protein.py
@@ -0,0 +1,28 @@
+import re
+import os
+os.chdir("/Users/lizhengxun/Documents/GitHub/IBI1_2020-21/Practical8")
+code = {
+    "TTT":'F',"TTC":'F',"TTA":'L',"TTG":'L',
+    "TCT":'S',"TCC":'S',"TCA":'S',"TCG":'S',
+    "TAT":'Y',"TAC":'Y',"TAA":'O',"TAG":'U',
+    "TGT":'C',"TGC":'C',"TGA":'X',"TGG":'W',
+    "CTT":'L',"CTC":'L',"CTA":'L',"CTG":'L',
+    "CCT":'P',"CCC":'P',"CCA":'P',"CCG":'P',
+    "CAT":'H',"CAC":'H',"CAA":'Q',"CAG":'Z',
+    "CGT":'R',"CGC":'R',"CGA":'R',"CGG":'R',
+    "ATT":'I',"ATC":'I',"ATA":'J',"ATG":'M',
+    "ACT":'T',"ACC":'T',"ACA":'T',"ACG":'T',
+    "AAT":'N',"AAC":'B',"AAA":'K',"AAG":'K',
+    "AGT":'S',"AGC":'S',"AGA":'R',"AGG":'R',
+    "GTT":'V',"GTC":'V',"GTA":'V',"GTG":'V',
+    "GCT":'A',"GCC":'A',"GCA":'A',"GCG":'A',
+    "GAT":'D',"GAC":'D',"GAA":'E',"GAG":'E',
+    "GGT":'G',"GGC":'G',"GGA":'G',"GGG":'G',
+}
+seq = 'ATGCGACTACGATCGAGGGCC'
+series = re.findall(r'...+?',seq)
+amino_series = []
+for i in range(len(series)):
+    amino =code[series[i]]
+    amino_series.append(amino)
+print(amino_series)
