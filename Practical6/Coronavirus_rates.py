424ccf8d064c039c7dd34edb4ac68a11782ae4da
lizhengxun <zhengxun.20@intl.zju.edu.cn>
2021-05-17 18:42:35 +0800

    ibi

--- Practical6/Coronavirus_rates.py
+++ Practical6/Coronavirus_rates.py
@@ -2,8 +2,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 # make a dictionary to store the data
 injections = {'USA':29862124, 'India':11285561, 'Brazil':11205972, 'Russia':436}
-# print the dictionary
-print(injections)
 values = [29862124, 11285561, 11205972, 4360823, 4234924]
 # calculate the percentage
 pro = []
@@ -14,6 +12,11 @@ for j in range(0,5):
 	a = float(values[j]/sum)
 	pro.append(a)
 # make a pie chart
+n = 0
+for country in injections:
+	injections[country] = pro[n]
+	n += 1
+print(injections)
 labels ='USA', 'India', 'Brazil', 'Russia','UK'
 colors = 'green', 'yellow', 'blue', 'red', 'pink'
 plt.pie(pro, labels=labels, colors=colors, autopct='%1.1f%%',startangle=90)
